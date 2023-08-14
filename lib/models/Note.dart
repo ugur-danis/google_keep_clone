aa79b54f5f4eb0eadbb68c0aa71040fc36f514a6
Uğur Danış <ugur.danis@detaysoft.com>
2023-08-14 14:48:26 +0300

    note model created

---
+++ lib/models/Note.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: file_names
+
+class Note {
+  final String title;
+  final String note;
+  final DateTime lastEditDate;
+
+  Note({
+    required this.title,
+    required this.note,
+    required this.lastEditDate,
+  });
+}

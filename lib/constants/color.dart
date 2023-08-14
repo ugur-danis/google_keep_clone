837405aa16fab9c1656284f29a2d58f3e47977a4
Uğur Danış <ugur.danis@detaysoft.com>
2023-08-14 13:50:00 +0300

    home screen header created

---
+++ lib/constants/color.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static const Color backgroundColor = Color(0xff1F1F1F);
+  static const Color bottomAppBarBg = Color(0xff2B2D2E);
+  static const Color floatingActionButtonBg = Color(0xFF3F4243);
+  static const Color secondaryTextColor = Color(0xffeeeee4);
+  static const Color drawerActiveTileColor = Color(0xff074B76);
+  static const Color drawerTextColor = Color(0xffc7eaff);
+  static const Color drawerIconColor = Color(0xffc7eaff);
+  static const Color actionsColor = Color(0xffd6d6cd);
+  static const Color userMenuContainerBg = Color(0xff3B3C41);
+  static const Color userMenuContentBg = Color(0xff2D2E30);
+  static const Color appBarBg = Color(0xff2B2C2E);
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:google_keep_clone/constants/color.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        key: _scaffoldKey,
+        resizeToAvoidBottomInset: false,
+        body: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+          header(),
+        ]),
+        drawer: const Drawer(),
+      ),
+    );
+  }
+
+  Widget header() {
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        margin: const EdgeInsets.only(top: 10, bottom: 20, left: 20, right: 20),
+        child: AppBar(
+          primary: true,
+          scrolledUnderElevation: 0,
+          elevation: 0,
+          toolbarHeight: 50,
+          backgroundColor: CustomColors.bottomAppBarBg,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(30)),
+          ),
+          titleSpacing: 0,
+          title: const Text(
+            'Notlarınızda arayın',
+            style: TextStyle(color: CustomColors.actionsColor, fontSize: 14),
+          ),
+          actions: [
+            switchViewButton(),
+            const SizedBox(width: 10),
+            avatar(),
+            const SizedBox(width: 10),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget avatar() {
+    return GestureDetector(
+      onTap: () {},
+      child: const CircleAvatar(
+        radius: 16,
+        backgroundImage: AssetImage('assets/images/profile-img.png'),
+      ),
+    );
+  }
+
+  IconButton switchViewButton() {
+    return IconButton(
+        onPressed: () {},
+        icon: const Icon(
+          Icons.view_agenda_outlined,
+          color: CustomColors.actionsColor,
+        ));
+  }
+}

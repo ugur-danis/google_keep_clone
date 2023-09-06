e212765e138a14f7800b244408745eb841e7ec65
ugur-danis <ugurdnis@gmail.com>
2023-09-06 11:27:03 +0300

    new note screen wrapped with safe area

--- lib/screens/new_note_screen.dart
+++ lib/screens/new_note_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_keep_clone/providers/note_provider.dart';
 import 'package:provider/provider.dart';
 
 import '../providers/note_provider.dart';
@@ -58,72 +57,76 @@ class _NewNoteState extends State<NewNote> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.push_pin_outlined),
-          ),
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.notification_add_outlined),
-          ),
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.archive_outlined),
-          ),
-        ],
-      ),
-      bottomNavigationBar: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.add_box_outlined),
-          ),
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.color_lens_outlined),
-          ),
-          Expanded(
-            child: Text(
-              'Düzenlenme saati: 15:06',
-              textAlign: TextAlign.center,
-              style:
-                  Theme.of(context).textTheme.bodySmall!.copyWith(fontSize: 10),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.push_pin_outlined),
             ),
-          ),
-          const SizedBox(width: 48),
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.more_vert),
-          ),
-        ],
-      ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          children: [
-            TextField(
-              controller: _titleEditingController,
-              style: Theme.of(context).textTheme.titleLarge!,
-              maxLines: null,
-              decoration: const InputDecoration(
-                hintText: 'Başlık',
-                border: InputBorder.none,
-              ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.notification_add_outlined),
             ),
-            TextField(
-              controller: _noteEditingController,
-              maxLines: null,
-              decoration: const InputDecoration(
-                hintText: 'Not',
-                border: InputBorder.none,
-              ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.archive_outlined),
             ),
           ],
+        ),
+        bottomNavigationBar: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.add_box_outlined),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.color_lens_outlined),
+            ),
+            Expanded(
+              child: Text(
+                'Düzenlenme saati: 15:06',
+                textAlign: TextAlign.center,
+                style: Theme.of(context)
+                    .textTheme
+                    .bodySmall!
+                    .copyWith(fontSize: 10),
+              ),
+            ),
+            const SizedBox(width: 48),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.more_vert),
+            ),
+          ],
+        ),
+        body: SingleChildScrollView(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              TextField(
+                controller: _titleEditingController,
+                style: Theme.of(context).textTheme.titleLarge!,
+                maxLines: null,
+                decoration: const InputDecoration(
+                  hintText: 'Başlık',
+                  border: InputBorder.none,
+                ),
+              ),
+              TextField(
+                controller: _noteEditingController,
+                maxLines: null,
+                decoration: const InputDecoration(
+                  hintText: 'Not',
+                  border: InputBorder.none,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );

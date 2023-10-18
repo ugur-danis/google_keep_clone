9d7ae0c301892ecaf67ec91974426528d506b4e0
ugur-danis <ugurdnis@gmail.com>
2023-10-18 12:16:15 +0300

    home screen refactoring

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -6,6 +6,7 @@ import 'package:provider/provider.dart';
 
 import '../../main.dart';
 import '../../models/Note.dart';
+import '../../services/archive/interfaces/IFirebaseArchiveManager.dart';
 import '../../services/note/interfaces/IFirebaseNoteManager.dart';
 import '../../providers/auth_provider.dart';
 import '../../widgets/drawer_menu.dart';
@@ -31,16 +32,43 @@ class _HomeScreenState extends State<HomeScreen> with _HomeScreenMixin {
       child: GestureDetector(
         onTap: focusClear,
         child: Scaffold(
-          resizeToAvoidBottomInset: false,
+          resizeToAvoidBottomInset: true,
+          floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
           drawer: const DrawerMenu(screen: DrawerMenuScreens.notes),
           bottomNavigationBar: const _BottomBar(),
           floatingActionButton: buildFloatingButton(),
-          floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
-          body: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              buildHeader(),
-              buildContent(),
+          appBar: _selectedNotes.isEmpty
+              ? buildDefaultAppBar()
+              : buildItemSelectedAppBar(),
+          body: buildContent(),
+        ),
+      ),
+    );
+  }
+
+  PreferredSize buildDefaultAppBar() {
+    return PreferredSize(
+      preferredSize: const Size.fromHeight(56 + 10), // with top padding
+      child: Padding(
+        padding: const EdgeInsets.only(left: 16, right: 16, top: 16),
+        child: GestureDetector(
+          onTap: navToSearchScreen,
+          child: AppBar(
+            scrolledUnderElevation: 0,
+            titleSpacing: 0,
+            shape: const StadiumBorder(),
+            title: Text(
+              'Search in your notes',
+              style: Theme.of(context)
+                  .textTheme
+                  .bodySmall!
+                  .copyWith(color: Theme.of(context).colorScheme.secondary),
+            ),
+            actions: [
+              buildSwitchViewButton(),
+              const SizedBox(width: 10),
+              buildAvatar(),
+              const SizedBox(width: 10),
             ],
           ),
         ),
@@ -48,31 +76,68 @@ class _HomeScreenState extends State<HomeScreen> with _HomeScreenMixin {
     );
   }
 
-  Widget buildHeader() {
-    return GestureDetector(
-      onTap: navToSearchScreen,
-      child: Container(
-        margin: const EdgeInsets.only(top: 10, bottom: 20, left: 20, right: 20),
-        child: AppBar(
-          primary: true,
-          scrolledUnderElevation: 0,
-          shape: const StadiumBorder(),
-          titleSpacing: 0,
-          title: Text(
-            'Notlarınızda arayın',
-            style: Theme.of(context)
-                .textTheme
-                .bodySmall!
-                .copyWith(color: Theme.of(context).colorScheme.secondary),
-          ),
-          actions: [
-            buildSwitchViewButton(),
-            const SizedBox(width: 10),
-            buildAvatar(),
-            const SizedBox(width: 10),
-          ],
-        ),
+  AppBar buildItemSelectedAppBar() {
+    final List<PopupMenuEntry> popupMenuItems = [
+      PopupMenuItem(
+        onTap: archiveNotes,
+        child: const Text('Archive'),
       ),
+      PopupMenuItem(
+        onTap: deleteNote,
+        child: const Text('Delete'),
+      ),
+    ];
+
+    if (_selectedNotes.length == 1) {
+      popupMenuItems.addAll([
+        PopupMenuItem(
+          onTap: createNoteCopy,
+          child: const Text('Create a copy'),
+        ),
+        PopupMenuItem(
+          onTap: () {},
+          child: const Text('Send'),
+        )
+      ]);
+    }
+
+    popupMenuItems.add(PopupMenuItem(
+      onTap: () {},
+      child: const Text('Copy to Google Docs'),
+    ));
+
+    return AppBar(
+      scrolledUnderElevation: 0,
+      titleSpacing: 0,
+      title: Text(
+        _selectedNotes.length.toString(),
+        style: Theme.of(context).textTheme.titleMedium,
+      ),
+      leading: IconButton(
+        onPressed: clearSelectedNotes,
+        icon: const Icon(Icons.close),
+      ),
+      actions: [
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.notifications_none),
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.notification_add_sharp),
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.color_lens_outlined),
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.label_outline),
+        ),
+        PopupMenuButton(
+          itemBuilder: (BuildContext context) => popupMenuItems,
+        ),
+      ],
     );
   }
 
@@ -84,7 +149,7 @@ class _HomeScreenState extends State<HomeScreen> with _HomeScreenMixin {
   }
 
   Widget buildAvatar() {
-    return GestureDetector(
+    return InkWell(
       onTap: () {
         showDialog(
           context: context,
@@ -103,29 +168,29 @@ class _HomeScreenState extends State<HomeScreen> with _HomeScreenMixin {
   }
 
   Widget buildContent() {
-    return StreamBuilder(
-      stream: _streamController.stream,
-      builder: (context, snapshot) {
-        if (snapshot.hasError) {
-          return Center(child: Text(snapshot.error.toString()));
-        } else if (snapshot.hasData) {
-          if (snapshot.data.length < 1) {
-            return buildNoNotesMessage(context);
-          }
-          return buildGridView(snapshot);
-        } else {
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        }
-      },
-    );
-  }
-
-  IllustratedMessage buildNoNotesMessage(BuildContext context) {
-    return const IllustratedMessage(
-      icon: Icons.lightbulb_outline,
-      text: 'Notes you add appear here',
+    return Padding(
+      padding: const EdgeInsets.only(top: 20),
+      child: Column(
+        children: [
+          StreamBuilder(
+            stream: _streamController.stream,
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                return Center(child: Text(snapshot.error.toString()));
+              } else if (snapshot.hasData) {
+                if (snapshot.data.length < 1) {
+                  return buildNoNotesMessage(context);
+                }
+                return buildGridView(snapshot);
+              } else {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+            },
+          ),
+        ],
+      ),
     );
   }
 
@@ -137,15 +202,34 @@ class _HomeScreenState extends State<HomeScreen> with _HomeScreenMixin {
         crossAxisCount: _gridCrossAxisCount,
         children: List.generate(
           snapshot.data.length,
-          (index) => NoteItem(note: snapshot.data[index]),
+          (index) => buildNoteItem(snapshot, index),
         ),
       ),
     );
   }
 
+  NoteItem buildNoteItem(AsyncSnapshot<dynamic> snapshot, int index) {
+    final Note note = snapshot.data[index];
+
+    return NoteItem(
+      note: note,
+      selected: _selectedNotes.contains(note),
+      onSelected: (bool isSelected) {
+        onChangeSelectedNotes(note, isSelected);
+      },
+    );
+  }
+
+  IllustratedMessage buildNoNotesMessage(BuildContext context) {
+    return const IllustratedMessage(
+      icon: Icons.lightbulb_outline,
+      text: 'Notes you add appear here',
+    );
+  }
+
   Widget buildFloatingButton() {
     return FloatingActionButton(
-      onPressed: navToNewNoteScreen,
+      onPressed: navToEditNoteScreen,
       shape: const CircleBorder(),
       child: SvgPicture.asset('assets/images/google-plus-icon.svg', width: 30),
     );
--- lib/screens/home/home_screen_model.dart
+++ lib/screens/home/home_screen_model.dart
@@ -2,7 +2,9 @@ part of 'home_screen.dart';
 
 mixin _HomeScreenMixin on State<HomeScreen> {
   late final IFirebaseNoteManager _noteManager;
+  late final IFirebaseArchiveManager _archiveManager;
   late final StreamController _streamController;
+  late final List<Note> _selectedNotes;
   int _gridCrossAxisCount = 2;
 
   @override
@@ -11,7 +13,9 @@ mixin _HomeScreenMixin on State<HomeScreen> {
 
     _noteManager = locator<IFirebaseNoteManager>();
     _noteManager.addListener(_handleNotesChange);
+    _archiveManager = locator<IFirebaseArchiveManager>();
     _streamController = StreamController();
+    _selectedNotes = [];
 
     fetchNotes();
   }
@@ -31,17 +35,37 @@ mixin _HomeScreenMixin on State<HomeScreen> {
     }
   }
 
+  void onChangeSelectedNotes(Note note, bool isSelected) {}
+
+  void clearSelectedNotes() {}
+
   void focusClear() => FocusScope.of(context).unfocus();
 
-  void navToSearchScreen() => Navigator.of(context)
-      .push(MaterialPageRoute(builder: (context) => const SearchNoteScreen()));
+  void archiveNotes() {}
 
-  void navToNewNoteScreen() => Navigator.of(context)
-      .push(MaterialPageRoute(builder: (context) => const EditNoteScreen()));
+  void deleteNote() {}
 
-  void toggleGridCrossAxisCount() => setState(() {
-        _gridCrossAxisCount = _gridCrossAxisCount == 2 ? 1 : 2;
-      });
+  void createNoteCopy() async {}
+
+  void navToSearchScreen() {
+    Navigator.of(context).push(
+        MaterialPageRoute(builder: (context) => const SearchNoteScreen()));
+  }
+
+  void navToEditNoteScreen() {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (context) => const EditNoteScreen()));
+  }
+
+  void toggleGridCrossAxisCount() {
+    setState(() {
+      _gridCrossAxisCount = _gridCrossAxisCount == 2 ? 1 : 2;
+    });
+  }
+
+  void notifyNoteDeleted(List<Note> deletedNotes) {}
+
+  void notifyNoteArchived(List<Note> archivedNotes) {}
 
   void _handleNotesChange(List<Note> notes) => _streamController.add(notes);
 }
